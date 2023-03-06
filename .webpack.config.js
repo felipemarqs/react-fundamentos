aa504eb1f9a872de878892a039f57a6b160655c9
Felipe Marques <felipe.marqs@outlook.com>
2023-03-06 18:37:40 +0000

    deleted:    .webpack.config.js 	new file:   build/bundled9266245957910624dca.js 	new file:   build/bundled9266245957910624dca.js.LICENSE.txt 	new file:   build/index.html 	deleted:    build/index.js 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	new file:   webpack.config.js

--- .webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-    entry: path.resolve(__dirname, 'transpiled' , 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-};
