12f512c6365c0d05515836102773b26ec7046560
Felipe Marques <felipe.marqs@outlook.com>
2023-03-10 19:19:51 +0000

    style: :art: Themes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+
+import { ThemeProvider } from "styled-components";
+
 import GlobalStyle from './styles/global.js'
 import Layout from './components/Layout/index.jsx'
 
+
+import themes from './styles/themes/index.js'
+
+console.log(themes)
+
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={themes.light}>
       <GlobalStyle/>
       <Layout/>
-    </>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styles/themes/common.js
@@ -0,0 +1,9 @@
+export default {
+    spacing: {
+      small: 8,
+      medium: 16,
+      large: 24,
+    },
+    borderRadius: '10px',
+  };
+  
