5cf06a254e21d3226759d34757e2c5754ebd9db1
Felipe Marques <felipe.marqs@outlook.com>
2023-03-13 12:00:46 -0300

    Params and Query

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Posts from "./pages/Posts/index.jsx";
 import Post from "./pages/Post/index.jsx";
 import Home from "./pages/Home/index.jsx";
+import NotFound from './pages/NotFound/index.jsx'
 
 const Routes = () => {
   return (
@@ -10,7 +11,8 @@ const Routes = () => {
       <>
         <Route exact path="/" component={Home} />
         <Route exact path="/posts" component={Posts} />
-        <Route path="/posts/id" component={Post} />
+        <Route path="/posts/:id" component={Post} />
+        <Route component={NotFound}/>
       </>
     </>
   );
